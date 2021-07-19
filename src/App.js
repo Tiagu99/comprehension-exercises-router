22640800b37f93e6c88ec7950ce168d9a4ca796f
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-19 09:20:38 -0300

    implementa navegaçao no app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        
+      </BrowserRouter>
     );
   }
 }
