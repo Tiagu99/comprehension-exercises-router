91b89ea67104dd5c86be8b40300fc4237f1c78e4
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-19 10:00:25 -0300

    implementa switch nas rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
@@ -11,9 +11,11 @@ class App extends Component {
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/users'>Users</Link>
-        <Route exact path="/" component= { Home } />
-        <Route path="/about" component= { About } />
-        <Route path="/users/:id" render={ (props) => <Users {...props} greetingsMessage="Good Moring" /> } />
+        <Switch>
+          <Route path="/users/:id" render={ (props) => <Users {...props} greetingsMessage="Good Moring" /> } />
+          <Route path="/about" component= { About } />
+          <Route exact path="/" component= { Home } />
+        </Switch>
       </BrowserRouter>
     );
   }
