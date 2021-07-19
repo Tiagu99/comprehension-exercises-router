66acb61c37f1f130b3adaf6edfe0d38f879655e0
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-19 09:29:47 -0300

    implementa links de navegacao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
@@ -8,6 +8,9 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/users'>Users</Link>
         <Route exact path="/" component= { Home } />
         <Route path="/about" component= { About } />
         <Route path="/users" component={ Users } />
